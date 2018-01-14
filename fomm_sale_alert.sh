039d1b117086ca4d7ae4116063d0c0aa21760593
Alvaro Martinez Llamojha <amllamojha@gmail.com>
2018-01-14 21:24:26 +0000

    removing old version

--- fomm_sale_alert.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DATE=$(date +"%Y-%m-%d-%H%M")
-TO="YOUR_EMAIL"
-SIZE="S"
-
-python fomm.py --size ${SIZE} > fomm.html
-
-cat fomm.html |  /usr/bin/mail -a  "Content-type: text/html;"  -s "FOMM ON SALE - ${DATE}" -aFrom:SizedFOMMServer\<noreply@llamojha.com\> ${TO}
